1fa4c0ef3c0800267365bbce33b26df2e93cf2d6
Tim Kiely <timothy.j.kiely@gmail.com>
2020-03-20 12:44:06 -0400

    Update final_housekeeping.sql

--- final_housekeeping.sql
+++ final_housekeeping.sql
@@ -6,11 +6,9 @@ analyze;
 -- Last runtime: 1 hr 28 min
 UNLOAD ('select * from nyc_fhv_data_v002')
 TO 's3://taxi.data.bak/FHV/FHV_'
-credentials 'aws_access_key_id=AKIAIRSVNG47S53REZNQ;aws_secret_access_key=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'
-GZIP;
+credentials #
 
 
 UNLOAD ('select * from nyc_taxi_data_v002')
 TO 's3://taxi.data.bak/MAIN/MAIN_'
-credentials 'aws_access_key_id=AKIAIRSVNG47S53REZNQ;aws_secret_access_key=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'
-GZIP;
+credentials #
